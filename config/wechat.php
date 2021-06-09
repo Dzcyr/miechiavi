f3470d517d35e021c915457dc96e5d1b8dd3e1e2
佟飞 <tongfei@yinchuangyun.cn>
2021-06-09 14:31:59 +0800

    feat:添加微信配置

--- config/wechat.php
+++ config/wechat.php
@@ -94,30 +94,32 @@ return [
     /*
      * 小程序
      */
-    // 'mini_program' => [
-    //     'default' => [
-    //         'app_id'  => env('WECHAT_MINI_PROGRAM_APPID', ''),
-    //         'secret'  => env('WECHAT_MINI_PROGRAM_SECRET', ''),
-    //         'token'   => env('WECHAT_MINI_PROGRAM_TOKEN', ''),
-    //         'aes_key' => env('WECHAT_MINI_PROGRAM_AES_KEY', ''),
-    //     ],
-    // ],
+    'mini_program' => [
+        'default' => [
+            'app_id'  => env('WECHAT_MINI_PROGRAM_APPID', ''),
+            'secret'  => env('WECHAT_MINI_PROGRAM_SECRET', ''),
+            'token'   => env('WECHAT_MINI_PROGRAM_TOKEN', ''),
+            'aes_key' => env('WECHAT_MINI_PROGRAM_AES_KEY', ''),
+        ],
+    ],
 
     /*
      * 微信支付
      */
-    // 'payment' => [
-    //     'default' => [
-    //         'sandbox'            => env('WECHAT_PAYMENT_SANDBOX', false),
-    //         'app_id'             => env('WECHAT_PAYMENT_APPID', ''),
-    //         'mch_id'             => env('WECHAT_PAYMENT_MCH_ID', 'your-mch-id'),
-    //         'key'                => env('WECHAT_PAYMENT_KEY', 'key-for-signature'),
-    //         'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'path/to/cert/apiclient_cert.pem'),    // XXX: 绝对路径！！！！
-    //         'key_path'           => env('WECHAT_PAYMENT_KEY_PATH', 'path/to/cert/apiclient_key.pem'),      // XXX: 绝对路径！！！！
-    //         'notify_url'         => 'http://example.com/payments/wechat-notify',                           // 默认支付结果通知地址
-    //     ],
-    //     // ...
-    // ],
+    'payment' => [
+        'default' => [
+            'sandbox' => env('WECHAT_PAYMENT_SANDBOX', false),
+            'app_id'  => env('WECHAT_PAYMENT_APPID', ''),
+            'mch_id' => env('WECHAT_PAYMENT_MCH_ID', ''),
+            'key' => env('WECHAT_PAYMENT_KEY', ''),
+            // Cert 绝对路径
+            'cert_path' => resource_path(env('WECHAT_PAYMENT_CERT_PATH', '')),
+            // Key 绝对路径
+            'key_path' => resource_path(env('WECHAT_PAYMENT_KEY_PATH', '')),
+            // 默认支付结果通知地址
+            'notify_url' => env('WECHAT_PAYMENT_NOTIFY_URL', ''),
+         ],
+    ],
 
     /*
      * 企业微信
