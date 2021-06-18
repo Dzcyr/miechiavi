340adf24d85774c92e58fd967795f1cfd4c8959a
tongfei <329636491@qq.com>
2021-06-18 14:17:49 +0800

    fix:修改默认为新上

--- app/Enums/HousingStatus.php
+++ app/Enums/HousingStatus.php
@@ -6,23 +6,23 @@ use BenSampo\Enum\Enum;
 
 final class HousingStatus extends Enum
 {
-    const DEFAULT = 1;
-    const SELECT = 2;
-    const RECOMMEND = 3;
+	const DEFAULT = 1;
+	const SELECT = 2;
+	const RECOMMEND = 3;
 
-    public static function getDescription($value): string
+	public static function getDescription($value): string
 	{
 		switch ($value) {
 			case self::DEFAULT:
-				return '默认';
+				return '新上';
 				break;
 			case self::SELECT:
 				return '精选';
-                break;
-            case self::RECOMMEND:
+				break;
+			case self::RECOMMEND:
 				return '推荐';
-                break;
+				break;
 		}
-	    return parent::getDescription($value);
+		return parent::getDescription($value);
 	}
 }
